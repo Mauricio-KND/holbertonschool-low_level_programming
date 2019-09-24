bdb49c41e700121e77a008427a8bb694033e8473
Mauricio-KND <1299@holbertonschool.com>
2019-09-24 21:14:20 +0000

    finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,36 @@
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
- *main - Fibonacci sequence
+ * main - print fibonacci numbers up to 98 numbers total
  *
- *Description: Prints the first 98 Fibonacci numbers
- *Return: 0 upon successful completion
+ * Return: Always 0.
  */
 int main(void)
 {
-int nthfibonacci = 3;
-double fibonacci;
-double nminus2 = 1;
-double nminus1 = 2;
+  int i = 0;
+  unsigned long n1, n2, quotient1, quotient2, rem1, rem2;
 
-printf("%.0f, ", nminus2);
-printf("%.0f, ", nminus1);
-
-while (nthfibonacci <= 98)
-{
-fibonacci = nminus2 + nminus1;
-printf("%.0f", fibonacci);
-if (nthfibonacci < 98)
-printf(", ");
-nminus2 = nminus1;
-nminus1 = fibonacci;
-nthfibonacci++;
-}
-printf("\n");
-return (0);
+  n1 = 1, n2 = 2;
+  while (i < 90)
+    {
+      printf("%lu, %lu, ", n1, n2);
+      n1 += n2, n2 += n1, i += 2;
+    }
+  printf("%lu, %lu, ", n1, n2);
+  quotient1 = n1 / 100, quotient2 = n2 / 100;
+  rem1 = n1 % 100, rem2 = n2 % 100;
+  while (i < 96)
+    {
+      n1 = quotient1 + quotient2, n2 = rem1 + rem2;
+      n2 > 99 ? n1++ : n2;
+      n2 = n2 % 100;
+      printf("%lu", n1);
+      printf(n2 < 10 ? "0" : "");
+      printf("%lu", n2);
+      printf(i < 95 ? ", " : "\n");
+      quotient1 = quotient2, quotient2 = n1;
+      rem1 = rem2, rem2 = n2, i++;
+    }
+  return (0);
 }
