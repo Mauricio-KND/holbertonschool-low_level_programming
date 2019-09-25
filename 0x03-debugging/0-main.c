143b571a08fd70585446259d5cd39a8c47423502
Mauricio-KND <1299@holbertonschool.com>
2019-09-25 01:37:53 +0000

    Based on the main.c file above, create a file named 0-main.c. This file must test that the function positive_or_negative() gives the correct output when given a case of 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,3 @@
-  
 #include "holberton.h"
 
 /**
@@ -8,11 +7,11 @@
 
 int main(void)
 {
-  int i;
+int i;
 
-  i = 0;
-  positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-  return (0);
+return (0);
 }
 
