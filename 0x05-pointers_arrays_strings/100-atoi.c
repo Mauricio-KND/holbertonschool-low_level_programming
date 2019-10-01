c7d7ac8ae1d46456d1bd00050589582707c47906
Mauricio-KND <1299@holbertonschool.com>
2019-10-01 18:05:12 +0000

    For the advanced one

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+  
+/**
+ * _atoi - takes a pointer to a string and converts it to an integer.
+ *
+ * @s: pointer to string.
+ *
+ * Return: signed int representation of string
+ */
+int _atoi(char *s)
+{
+  int p = 1;
+  unsigned int ret = 0;
+
+  while ((*s != '\0') && !(*s >= '0' && *s <= '9'))
+    if (*s++ == '-')
+      p *= -1;
+  while ((*s != '\0') && (*s >= '0' && *s <= '9'))
+    ret = ret * 10 + (*s++ - '0');
+
+  return (p * ret);
+}
