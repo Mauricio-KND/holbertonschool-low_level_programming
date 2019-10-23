eab0a68f3de2c29a7ed422671d0a0dff04db8ae2
Mauricio-KND <1299@holbertonschool.com>
2019-10-23 18:01:03 +0000

    Corrected one. Prints a name.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef _FUNC_POINTERS_
+#define _FUNC_POINTERS_
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
