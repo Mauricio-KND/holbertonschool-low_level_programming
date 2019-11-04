7bc1d54ce781ac7b0754227673e889a2149c80b6
Mauricio-KND <1299@holbertonschool.com>
2019-11-04 18:27:39 +0000

    Returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,6 +10,7 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
+
 typedef struct listint_s
 {
 	int n;
@@ -17,5 +19,6 @@ typedef struct listint_s
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
