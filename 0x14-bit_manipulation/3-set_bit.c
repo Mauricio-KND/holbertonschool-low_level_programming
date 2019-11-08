5a7e1af68d4ebed6cb8dbac64b6463777b26953c
Mauricio-KND <1299@holbertonschool.com>
2019-11-08 02:15:51 +0000

    Task number 3.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * set_bit - Sets the value of a bit to 1 at a given index.
+ * @n: number to set bit in
+ * @index: index to set bit
+ * Return: 1 on success, -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int sz;/*Size of binary number*/
+
+	sz = sizeof(*n) * 8 - 1;
+	if (index > sz)
+		return (-1);
+	*n = (1 << index) | *n;
+	return (1);
+}
