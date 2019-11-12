4037e8c52f6faebd669c0e19526d5f3c46ad662e
Mauricio-KND <1299@holbertonschool.com>
2019-11-12 18:45:40 +0000

    Task number 0.

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
