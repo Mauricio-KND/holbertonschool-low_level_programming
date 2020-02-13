bd4d70c6a02216724f428d471d48cb326ff860c1
Mauricio-KND <1299@holbertonschool.com>
2020-02-13 16:46:47 +0000

    Keep Pushing.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Prints all possible combinations of single-digit numbers.
+ * Return: An integer.
  */
 int main(void)
 {
-int i;
-i = 0;
-while (i < 10)
-{
-putchar(i % 10 + '0');
-if (i < 9)
-{
-putchar(',');
-putchar(' ');
-}
-i++;
-}
-putchar('\n');
-return (0);
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			/* Prints comma and space until 9. */
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	/* The new line after complete the loop. */
+	putchar('\n');
+	return (0);
 }
