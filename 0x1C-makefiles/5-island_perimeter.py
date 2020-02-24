0aeeecc67d03331bd2ba66ea155efd8c922a0c8b
Mauricio-KND <1299@holbertonschool.com>
2020-02-24 03:05:31 +0000

    Improvising

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Initializes island_perimeter function. """
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid."""
+    width = 3
+    height = 3
+
+    return (2 * height + 2 * width)
